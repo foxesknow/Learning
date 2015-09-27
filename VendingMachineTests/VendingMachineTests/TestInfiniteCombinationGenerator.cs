4ebd879f2b05790020646a20290a9715ac7d0677
foxesknow <seanbatten58@gmail.com>
2015-09-27 20:41:27 +0100

    Added tests

---
+++ VendingMachineTests/VendingMachineTests/TestInfiniteCombinationGenerator.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Diagnostics;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using VendingMachineTests.DynamicProgramming;
+
+namespace VendingMachineTests
+{
+	[TestClass]
+	public class TestInfiniteCombinationGenerator
+	{
+		[TestMethod]
+		public void ZeroPence()
+		{
+			var generator=new InfiniteCombinationGenerator(1, 2, 3);
+
+			int combinations=0;
+
+			generator.Generate(0, change=>
+			{
+				var text=string.Join(", ", change.Collapse());
+				Console.WriteLine(text);
+
+				combinations++;
+			});
+
+			Assert.IsTrue(combinations==0);
+		}
+
+		[TestMethod]
+		public void FourPence()
+		{
+			var generator=new InfiniteCombinationGenerator(1, 2, 3);
+
+			int combinations=0;
+
+			generator.Generate(4, change=>
+			{
+				var text=string.Join(", ", change.Collapse());
+				Console.WriteLine(text);
+
+				combinations++;
+			});
+
+			Assert.IsTrue(combinations==4);
+		}
+
+		[TestMethod]
+		public void TenPence()
+		{
+			var generator=new InfiniteCombinationGenerator(1, 2, 3);
+
+			int combinations=0;
+
+			generator.Generate(10, change=>
+			{
+				var text=string.Join(", ", change.Collapse());
+				Console.WriteLine(text);
+
+				combinations++;
+			});
+
+			Assert.IsTrue(combinations==14);
+		}
+
+	}
+}
