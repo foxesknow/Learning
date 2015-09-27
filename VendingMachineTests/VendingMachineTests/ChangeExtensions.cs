6dd86a7fc70d142d5b065336aaad227cf24481dc
foxesknow <seanbatten58@gmail.com>
2015-09-27 20:17:41 +0100

    Extensions

---
+++ VendingMachineTests/VendingMachineTests/ChangeExtensions.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VendingMachineTests
+{
+	static class ChangeExtensions
+	{
+		public static IEnumerable<Change> Collapse(this IEnumerable<Change> change)
+		{			
+            return change.GroupBy(c => c.Coin)
+                    .Select(group => new Change(group.Key, group.Sum(c=>c.Quantity)));
+		}
+	}
+}
