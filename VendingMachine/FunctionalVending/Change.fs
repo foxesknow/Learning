ecab8d606d0a67e8589a8ee6aa0d2c86a8225520
Sean Batten <seanbatten58@gmail.com>
2017-03-26 19:53:30 +0100

    Started F# implemenation to help learn the language

---
+++ VendingMachine/FunctionalVending/Change.fs
@@ -0,0 +1,42 @@
+﻿namespace FunctionalVending
+
+module Change =
+
+    type T = {Denomination : int; Quantity : int}
+
+    let create denomination quantity =
+        {Denomination = denomination; Quantity = quantity}
+
+    let totalValue change =
+        change.Denomination * change.Quantity
+
+    let (++) change quantity =
+        let newQuantity = change.Quantity + quantity
+
+        if(newQuantity < 1) then
+            invalidArg "quantity" "new quantity would be 0 or less"
+        else
+            {change with Quantity = newQuantity}
+
+    let (+++) change changeToAdd =
+        if change.Denomination <> changeToAdd.Denomination then
+            invalidArg "changeToAdd" "denominations do not match"
+        else
+            {change with Quantity = change.Quantity + changeToAdd.Quantity}
+
+    //let totalValueOf changes =
+        //List.sumBy (fun c -> totalValue c) changes
+
+    let totalValueOf changes =
+        let rec loop acc = function
+            | [] -> acc
+            | x::xs -> loop (acc + totalValue x) xs
+
+        loop 0 changes
+
+
+
+        
+
+ 
+
---
+++ VendingMachine/FunctionalVending/Program.fs
@@ -0,0 +1,10 @@
+﻿namespace FunctionalVending
+
+open Change
+
+module Program =
+    [<EntryPoint>]
+    let main argv = 
+        let c = [create 1 10; create 5 1]
+        printfn "%A" (totalValueOf c)
+        0 // return an integer exit code
