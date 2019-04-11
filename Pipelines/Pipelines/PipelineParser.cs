f4381b828e132de1f0318478e28d03d23e3d6518
Sean Batten <seanbatten58@gmail.com>
2019-04-11 21:13:49 +0100

    Initial checkin

---
+++ Pipelines/Pipelines/PipelineParser.cs
@@ -0,0 +1,153 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Pipelines
+{
+    static class PipelineParser
+    {
+        private static readonly char DoubleQuote = '"';
+        private static readonly char SingleQuote = '\'';
+        private static readonly char Pipe = '|';
+        private static readonly char Escape = '`';
+
+        public static IEnumerable<Token> Parse(string line)
+        {
+            bool inEscape = false;
+            bool inQuote = false;
+            var lastToken = TokenType.None;
+            char quoteBlockTerminator = '\0';
+
+            var active = "";
+
+            foreach(var c in line)
+            {
+                if(inQuote)
+                {
+                    if(inEscape)
+                    {
+                        active += ConvertEscape(c);
+                        inEscape = false;
+                        continue;
+                    }
+
+                    // Only escape text in double quotes, like Powershell
+                    if(c == Escape && quoteBlockTerminator == DoubleQuote)
+                    {
+                        inEscape = true;
+                        continue;
+                    }
+
+                    if(c == quoteBlockTerminator)
+                    {
+                        yield return MakeAndReset(TokenType.String);
+                        inQuote = false;
+                        quoteBlockTerminator = '\0';
+                        continue;
+                    }
+
+                    active += c;
+                }
+                else
+                {
+                    if(c == Escape) throw new Exception("can only escape in quoted text");
+
+                    if(c == ' ')
+                    {
+                        if(active.Length != 0)
+                        {
+                            yield return MakeAndReset(TokenType.String);
+                        }
+
+                        continue;
+                    }
+
+                    if(c == DoubleQuote || c == SingleQuote || c == Pipe)
+                    {
+                        if(active.Length != 0)
+                        {
+                            yield return MakeAndReset(TokenType.String);
+                        }
+
+                        if(c == Pipe)
+                        {
+                            if(lastToken != TokenType.String) throw new Exception("unexpected pipe");
+
+                            yield return MakeAndReset(TokenType.Pipe);
+                            continue;
+                        }
+
+                        inQuote = true;
+                        quoteBlockTerminator = c;
+
+                        continue;
+                    }
+
+                    active += c;
+                }
+            }
+
+            if(active.Length != 0)
+            {
+                yield return new Token(TokenType.String, active);
+            }
+            else
+            {
+                if(inQuote) throw new Exception("Unterminated string");
+                if(inEscape) throw new Exception("Unterminated escape sequence");
+                if(lastToken == TokenType.Pipe) throw new Exception("nothing to right of pipeline");
+            }            
+
+            Token MakeAndReset(TokenType type)
+            {
+                var token = new Token(type, active);
+                lastToken = type;
+                active = "";
+
+                return token;
+            }
+        }
+
+        private static char ConvertEscape(char c)
+        {
+            switch(c)
+            {
+                case 't':   return '\t';
+                case 'r':   return '\r';
+                case 'n':   return '\n';
+                default:    return c;
+            }
+        }
+    }
+
+    struct Token
+    {
+        public Token(TokenType type, string value)
+        {
+            this.Type = type;
+            this.Value = value;
+        }
+
+        public readonly TokenType Type;
+        public readonly string Value;
+
+        public override string ToString()
+        {
+            if(this.Type == TokenType.String)
+            {
+                return $"String = [{this.Value}]";
+            }
+            else
+            {
+                return this.Type.ToString();
+            }
+        }
+    }
+
+    enum TokenType
+    {
+        None,
+        Pipe,
+        String
+    }
+}
---
+++ Pipelines/Pipelines/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Pipelines
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                //Print(PipelineParser.Parse("dir *.txt"));
+                //Print(PipelineParser.Parse("dir | more"));
+                //Print(PipelineParser.Parse("dir|more"));
+                //Print(PipelineParser.Parse("  dir| \"hello`tthere\" "));
+                //Print(PipelineParser.Parse("dir *.txt | echo foo bar \"   1 2 3 4      \""));
+                //Print(PipelineParser.Parse("dir *.txt | more | top 5"));
+                Print(PipelineParser.Parse("                         "));
+            }
+            catch(Exception e)
+            {
+                Console.Error.WriteLine(e.Message);
+            }
+        }
+
+        static void Print(IEnumerable<Token> tokens)
+        {
+            foreach(var token in tokens)
+            {
+                Console.WriteLine(token);
+            }
+        }
+    }
+}
